ea1f26b51bc19b6812989bba16351c23a47c361c
egoncalv <erickbarros06@hotmail.com>
2022-04-28 04:22:52 +0100

    vish

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <egoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 01:10:32 by egoncalv          #+#    #+#             */
-/*   Updated: 2022/04/28 03:07:17 by egoncalv         ###   ########.fr       */
+/*   Updated: 2022/04/28 04:20:41 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	main(void)
 
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, 960, 540, "SO_LONG");
+	data.img = mlx_png_file_to_image(data.mlx, "../assets/Pink_Monster/Pink_Monster.png", &data.img_width, &data.img_heigth);
+	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 	return (0);
 }
