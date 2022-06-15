4e36b2b56e0850b5d3b3b92bcdf01bd15c32c989
Erick Gonçalves <erickbarros06@hotmail.com>
2022-06-15 16:52:00 +0000

    fix

--- src/utils.c
+++ src/utils.c
@@ -16,7 +16,7 @@ void	my_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
 	*(unsigned int *)dst = color;
 }
 
